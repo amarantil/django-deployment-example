5e47d0f4324d2e86788649757caa26d0723643b7
amarantil <amartt2174@gmail.com>
2020-05-23 10:19:41 +0530

    'second'

---
+++ learning_templates/basic_app/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from basic_app import views
+
+# TEMPLATE TAGGING
+app_name = 'basic_app'
+
+urlpatterns=[
+    url(r'^other/$',views.other,name='other'),
+    url(r'^relative_url_templates/$',views.relative_url_templates,name='relative_url_templates'),
+]
